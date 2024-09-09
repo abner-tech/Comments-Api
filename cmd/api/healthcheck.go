4d36b9c84bafcd3cf5c89415c3c0d9f0829e00c5
abner-tech <2020151792@ub.edu.bz>
2024-09-09 12:52:39 -0600

    json Support Update

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,12 @@ import (
 )
 
 func (a *applicationDependences) healthChechHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", a.config.environment)
-	fmt.Fprintf(w, "version: %s\n", appVersion)
+	// fmt.Fprintln(w, "status: available")
+	// fmt.Fprintf(w, "environment: %s\n", a.config.environment)
+	// fmt.Fprintf(w, "version: %s\n", appVersion)
+
+	jsResponse := `{"Status": "available","environment": %q, "Version":%q}`
+	jsResponse = fmt.Sprintf(jsResponse, a.config.environment, appVersion)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(jsResponse))
 }
