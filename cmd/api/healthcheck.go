5e3e7bd83a19a461081c42f3261f94a6b3bcc4b9
abner-tech <2020151792@ub.edu.bz>
2024-09-16 12:53:22 -0600

    new code

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,17 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
-func (a *applicationDependences) healthChechHandler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintln(w, "status: available")
-	// fmt.Fprintf(w, "environment: %s\n", a.config.environment)
-	// fmt.Fprintf(w, "version: %s\n", appVersion)
+// func (a *applicationDependences) healthChechHandler(w http.ResponseWriter, r *http.Request) {
+// 	// fmt.Fprintln(w, "status: available")
+// 	// fmt.Fprintf(w, "environment: %s\n", a.config.environment)
+// 	// fmt.Fprintf(w, "version: %s\n", appVersion)
 
-	jsResponse := `{"Status": "available","environment": %q, "Version":%q}`
-	jsResponse = fmt.Sprintf(jsResponse, a.config.environment, appVersion)
+// 	jsResponse := `{"Status": "available","environment": %q, "Version":%q}`
+// 	jsResponse = fmt.Sprintf(jsResponse, a.config.environment, appVersion)
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.Write([]byte(jsResponse))
+// }
+
+func (a *applicationDependences) healthChechHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"status":      "available",
+		"environment": a.config.environment,
+		"version":     appVersion,
+	}
+
+	jsResponse, err := json.Marshal(data)
+	if err != nil {
+		a.logger.Error(err.Error())
+		http.Error(w,
+			"The Server Encountered a problem and could not proccess your Request",
+			http.StatusInternalServerError)
+		return
+	}
+
+	jsResponse = append(jsResponse, '\n')
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(jsResponse))
+	w.Write(jsResponse)
 }
