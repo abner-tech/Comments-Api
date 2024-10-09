36e1a982a77b0e041952100ec8e87e9638bdcb12
abner-tech <2020151792@ub.edu.bz>
2024-10-09 14:25:56 -0600

    parsing-json-cient-request/v3

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
 type envelope map[string]any
@@ -36,11 +37,20 @@ func (a *applicationDependences) writeJSON(w http.ResponseWriter,
 }
 
 func (a *applicationDependences) readJSON(w http.ResponseWriter, r *http.Request, destination any) error {
-	err := json.NewDecoder(r.Body).Decode(destination)
+	//max size of the request body in this case is 250KB
+	maxBytes := 256_000
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	//decoder to check for unkown fields
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	//start decoding process
+	err := dec.Decode(destination)
+	//check for different errors
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
 		var invalidUnmarshalError *json.InvalidUnmarshalError
+		var maxBytesError *http.MaxBytesError
 
 		switch {
 		case errors.As(err, syntaxError):
@@ -49,11 +59,18 @@ func (a *applicationDependences) readJSON(w http.ResponseWriter, r *http.Request
 			return errors.New("The Body contains badly-formed JSOn")
 		case errors.As(err, &unmarshalTypeError):
 			if unmarshalTypeError.Field != "" {
-				return fmt.Errorf("The Body contains Incorrect JSON type for field %q", unmarshalTypeError.Field)
+				return fmt.Errorf("the body contains Incorrect JSON type for field %q", unmarshalTypeError.Field)
 			}
 			return fmt.Errorf("The body contains the incorrect JSON type at character: %d", unmarshalTypeError.Offset)
 		case errors.Is(err, io.EOF):
 			return errors.New("The body must not be empty")
+			//checking for unknown field errors
+		case strings.HasPrefix(err.Error(), "Json: unknown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unkown field ")
+			return fmt.Errorf("body containes unkown key %s", fieldName)
+		//check if body is grater than limit of 250KB
+		case errors.As(err, &maxBytesError):
+			return fmt.Errorf("the body must not be larger than %d bytes", maxBytesError.Limit)
 		//the program messed up
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
@@ -62,5 +79,11 @@ func (a *applicationDependences) readJSON(w http.ResponseWriter, r *http.Request
 			return err
 		}
 	}
+	err = dec.Decode(&struct{}{})
+	if !errors.Is(err, io.EOF) {
+		//more DATA is present
+		return errors.New("body must only contain a single JSON value")
+	}
+
 	return nil
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,9 @@ module github.com/abner-tech/Comments-Api.git
 
 go 1.23.0
 
+require golang.org/x/text v0.19.0
+
 require (
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	golang.org/x/text v0.19.0
+	github.com/lib/pq v1.10.9 // indirect
 )
